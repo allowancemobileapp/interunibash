b58826214bbc221c93b2a3055a02959a62ad0184
Brand Boost <brandboostfund@gmail.com>
2025-10-16 00:53:54 +0000

    The next layer after the text layer is a split layer. Half image and hal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { HighlightsSection } from '@/components/highlights-section';
 import { LineupSection } from '@/components/lineup-section';
 import { PromoSlider } from '@/components/promo-slider';
 import { TicketsSection } from '@/components/tickets-section';
+import { SplitSection } from '@/components/split-section';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection />
       <InfoSection />
+      <SplitSection />
       <PromoSlider />
       <HighlightsSection />
       <LineupSection />
